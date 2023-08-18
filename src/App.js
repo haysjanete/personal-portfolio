12eb4589e92ebc3e0bc9eeed72029e50e219ecfb
haysjanete <74339100+haysjanete@users.noreply.github.com>
2023-08-18 18:28:46 -0500

    created a new  react  app for portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className = "hero d-flex align-items-center justify-content-center">
+        <div className="video-wrap">
+          <video src= "https://tactusmarketing.com/wp-content/uploads/tactus-waves-hero.mp4" autoplay="{true}" loop muted className="rotate-180">
+          </video>
+        </div>
+        <div className="position-absolute w-100 gradient-overlay content-relative text-center"></div>
+        <div className="content-relative text-center mb-5">
+          <h1 className="mix-blend-difference text-2xl content-relative text-center position-absolute w-100 gradient-overlay mb-5">Janete Hays
+            <br />
+            Software Engineer
+          </h1>
+        </div>
+      </section>
+    </main>
+  )
 }
-
-export default App;
