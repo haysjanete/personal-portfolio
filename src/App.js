562355f263a5454e5e5d49b3e01a04527a3a3169
haysjanete <74339100+haysjanete@users.noreply.github.com>
2023-08-19 23:31:59 -0500

    added sections and css

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ export default function App() {
     <main>
       <section className = "hero d-flex align-items-center justify-content-center">
         <div className="video-wrap">
-          <video src= "https://tactusmarketing.com/wp-content/uploads/tactus-waves-hero.mp4" autoplay="{true}" loop muted className="rotate-180">
+          <video src= "https://tactusmarketing.com/wp-content/uploads/tactus-waves-hero.mp4" autoplay="{true}" loop muted className="rotate-180" id="video-bg">
           </video>
         </div>
-        <div className="position-absolute w-100 gradient-overlay content-relative text-center"></div>
-        <div className="content-relative text-center mb-5">
-          <h1 className="mix-blend-difference text-2xl content-relative text-center position-absolute w-100 mb-5">Janete Hays
+        <div className="position-absolute w-100 gradient-overlay"></div>
+        <div className="content position-relative text-center mb-5">
+          <h1 className="hero-title blend">Janete Hays
             <br />
             Software Engineer
           </h1>
         </div>
       </section>
+      <section className="two"></section>
+        <div className="text-lg ">About Me</div>
+          <p className=""></p>
+      <section className="three"></section>
+      <section className="four"></section>
+      <section className="five"></section>
     </main>
   )
 }
