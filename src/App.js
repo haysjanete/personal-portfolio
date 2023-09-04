bb31098bef272a29e88f39c06609d97800d86679
haysjanete <74339100+haysjanete@users.noreply.github.com>
2023-09-03 20:38:33 -0500

    made about me  responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 export default function App() {
   return (
     <main>
+    <div className="flex"></div>
       <section className = "hero d-flex align-items-center justify-content-center">
         <div className="video-wrap">
           <video src= "https://tactusmarketing.com/wp-content/uploads/tactus-waves-hero.mp4" autoplay="{true}" loop muted className="rotate-180" id="video-bg">
@@ -14,11 +15,14 @@ export default function App() {
           </h1>
         </div>
       </section>
-      <section className="two"></section>
-        <div className="hero d-flex align-items-center justify-content-center text-8xl font-bold">About Me</div>
-          <p className="hero d-flex align-items-center justify-content-center text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <img className="float-left" src="public/portrait.jpeg" alt="portrait"></img>
-          <p className=""></p>
+      <section className="flex align-items-center justify-content-center">
+        <div className="position-absolute w-100"></div>
+        <div className="content position-relative text-center mb-5"></div>
+            <h1 className="inline-block self-center text-center  text-auto font-bold">About Me
+            <br />
+              <p className="flex text-center text-xs sm:text-s md:text-2xl lg:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></h1>
+          <img className="float-left" src="public/portrait.jpeg" alt="portrait"></img>
+          </section>
       <section className="three"></section>
         <div className="portfolio">Portfolio Projects</div>
       <section className="four"></section>
